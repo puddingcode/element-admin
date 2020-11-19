bbc5238f5e89fb15865e51ce8e433ab21de1b102
unknown <l616769490@126.com>
2020-11-19 17:54:05 +0800

    new commit

--- src/store/index.js
+++ src/store/index.js
@@ -7,13 +7,13 @@ Vue.use(Vuex)
 const moduleFiles = require.context('./modules', true, /\.js$/)
 
 const modules = moduleFiles.keys().reduce((modules, modulePath) => {
-  const moduleName = modulePath.replace(/^\.\(.*)\.\w+$/, '$1')
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
   const value = moduleFiles(modulePath)
   modules[moduleName] = value.default
   return modules
 }, {})
 
-const store = new Vuex.store({
+const store = new Vuex.Store({
   modules,
   getters
 })
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -12,7 +12,81 @@
         <h3 class="title">{{ $t('login.title') }}</h3>
         <lang-select class="set-language"></lang-select>
       </div>
+
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon-class="user"></svg-icon>
+        </span>
+        <el-input
+          ref="username"
+          v-model="loginForm.username"
+          :placeholder="$t('login.username')"
+          type="text"
+          tabindex="1"
+          autocomplete="on"
+        ></el-input>
+      </el-form-item>
+
+      <el-tooltip
+        v-model="capsTooltip"
+        content="Caps loack is on"
+        placement="right"
+        manual
+      >
+        <el-form-item prop="password">
+          <span class="svg-container">
+            <svg-icon icon-class="password"></svg-icon>
+          </span>
+          <el-input
+            :key="passwordType"
+            ref="password"
+            v-model="loginForm.password"
+            :type="passwordType"
+            :placeholder="$t('login.password')"
+            name="password"
+            tabindex="2"
+            autocomplete="on"
+            @keyup.native="checkCapslock"
+            @blur="capsToltip = false"
+            @keyup.enter.native="handleLogin"
+          >
+          </el-input>
+          <span class="show-pwd" @click="showPwd">
+            <svg-icon
+              :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+            ></svg-icon>
+          </span>
+        </el-form-item>
+      </el-tooltip>
+
+      <el-button
+        :loading="loading"
+        type="primary"
+        style="width: 100%;margin-bottom:30px;"
+        @click.native.prevent="handleLogin"
+        >{{ $t('login.logIn') }}</el-button
+      >
+
+      <div style="position:relative">
+        <div class="tips">
+          <span>{{ $t('login.username') }}:admin</span>
+          <span>{{ $t('login.password') }}:{{ $t('login.any') }}</span>
+        </div>
+      </div>
+
+      <el-button
+        class="thirdparty-button"
+        type="primary"
+        @click="showDialog = true"
+      >
+        {{ $t('login.thirdparty') }}
+      </el-button>
     </el-form>
+
+    <el-dialog :title="$t('login.thirdparty')" :visible.sync="showDialog">
+      {{ $t('login.thirdparty') }}
+      <social-sign></social-sign>
+    </el-dialog>
   </div>
 </template>
 
